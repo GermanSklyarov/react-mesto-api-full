dd02b722e97530f996ab3b49dd2602409143f6cd
German <german.s.1993@yandex.ru>
2022-07-12 15:23:52 +0300

    решение проблемы с авторизацией

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -91,6 +91,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: 'none',
+          secure: true,
         });
       res.send({ token });
     })
